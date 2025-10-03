3e4fe3d75ed28d860bc0c7d8c8870bf1b7e765ca
EnvantageITSupport <it.support@Envantage.co.uk>
2025-10-03 14:34:05 +0100

    Update Apply-Wallpaper-User.ps1

--- Apply-Wallpaper-User.ps1
+++ Apply-Wallpaper-User.ps1
@@ -7,7 +7,8 @@ Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -Value
 
 # Force immediate refresh by killing and restarting explorer
 Stop-Process -Name explorer -Force
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 60
 
 # Explorer will restart automatically and load the new wallpaper
 Write-Host "Wallpaper applied"
+
